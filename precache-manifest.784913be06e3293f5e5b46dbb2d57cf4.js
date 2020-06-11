4a244cf733b7bfc497d3954e462699ba1e485550
ANNGUYEN <sieusaothu2@gmail.com>
2020-06-11 13:42:38 +0700

    Updates

---
+++ precache-manifest.784913be06e3293f5e5b46dbb2d57cf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fdc7a4b32d53faa95fd58c3aa40a9a",
+    "url": "/Alogbrain/WebRTC.git/index.html"
+  },
+  {
+    "revision": "52b8d43be3d669441be8",
+    "url": "/Alogbrain/WebRTC.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb47d15893d648dff8fc",
+    "url": "/Alogbrain/WebRTC.git/static/js/2.ce85243a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Alogbrain/WebRTC.git/static/js/2.ce85243a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b8d43be3d669441be8",
+    "url": "/Alogbrain/WebRTC.git/static/js/main.9a462218.chunk.js"
+  },
+  {
+    "revision": "6633c2fc1303b296b1dc",
+    "url": "/Alogbrain/WebRTC.git/static/js/runtime-main.c2061b28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Alogbrain/WebRTC.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
